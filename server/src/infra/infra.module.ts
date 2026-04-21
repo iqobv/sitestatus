38a757a1bad2888cecc651aab03eaac8560334eb
oberskyi_vitalii <oberskuyvitalik@gmail.com>
2026-04-21 21:01:56 +0300

    removed redis connections

--- server/src/infra/infra.module.ts
+++ server/src/infra/infra.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
-import { QueueModule } from './queue/queue.module';
 import { MailModule } from './mail/mail.module';
-import { RedisModule } from './redis/redis.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { ServiceBusModule } from './service-bus/service-bus.module';
 
 @Module({
-	imports: [PrismaModule, QueueModule, MailModule, RedisModule, ServiceBusModule],
+	imports: [PrismaModule, MailModule, ServiceBusModule],
 })
 export class InfraModule {}
